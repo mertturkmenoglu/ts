b90fa84721c9702ca36ded45ce7a24bec4fb3c44
Mert <mertturkmenoglu99@gmail.com>
2019-06-24 23:28:27 +0300

    Add quickSort implementation

---
+++ E005_QuickSort/main.ts
@@ -0,0 +1,40 @@
+/**
+ * QuickSort example
+ */
+
+function quickSort(array: Array<number>, begin: number = 0, end: number = array.length-1) {
+    if (begin < end) {
+        let pIndex = partition(array, begin, end)
+        quickSort(array, begin, pIndex - 1)
+        quickSort(array, pIndex + 1, end)
+    }
+}
+
+function partition(array: Array<number>, begin: number, end: number): number {
+    let pivot = array[end]
+    let i = begin - 1
+
+    for (let j = begin; j <= end - 1; j++) {
+        if (array[j] <= pivot) {
+            i++
+            let tmp = array[i]
+            array[i] = array[j]
+            array[j] = tmp
+        }
+    }
+
+    i++
+
+    let tmp = array[i]
+    array[i] = array[end]
+    array[end] = tmp
+
+    return i
+}
+
+
+let arr = [3, 2, 4, 4, 5, 1, 0, 2, 9, 5, 4, 6, 3]
+console.log(arr)
+quickSort(arr)
+console.log(arr)
+
